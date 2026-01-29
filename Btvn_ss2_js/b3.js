db39ec30189a28af78c0ba0b3ce57832abbe7d2c
trung2007hs-jpg <trung2007hs@gmail.com>
2026-01-29 22:03:28 +0700

    Update b3.js

--- Btvn_ss2_js/b3.js
+++ Btvn_ss2_js/b3.js
@@ -1,17 +1,18 @@
 //nhập thông tin
 let bookName = prompt("Nhập tên sách:");
-let category = prompt("Nhập thể loại sách:").toLowerCase();
-let status = prompt("Nhập tình trạng sách (co san / da muon):").toLowerCase();
+let category = prompt("Nhập thể loại sách:").trim().toLowerCase();
+let status = prompt("Nhập tình trạng sách (co san / da muon):").trim().toLowerCase();
 
-//kiểm tra và phân loại
-if (category === "khoa học" || category === "lịch sử") {
-    if (status === "co san") {
-        console.log("Sách này có sẵn trong thư viện");
-    } else {
-        console.log("Sách đã được mượn");
-    }
-} else if (category === "văn học" || category === "truyện") {
+// kiểm tra và phân loại
+if ((category === "khoa học" || category === "lịch sử") && status === "co san") {
+    console.log("Sách này có sẵn trong thư viện");
+}
+else if ((category === "khoa học" || category === "lịch sử") && status === "da muon") {
+    console.log("Sách đã được mượn");
+}
+else if (category === "văn học" || category === "truyện") {
     console.log("Sách này có thể đọc giải trí");
-} else {
+}
+else {
     console.log("Thể loại sách khác");
 }
